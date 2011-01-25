578e68383b8cb22e3c62264eacc30533747dbb1c
dariimd <dariimd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-25 21:07:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/oqey-gallery/tags/0.3.2@337225 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ managemusic.php
@@ -0,0 +1,210 @@
+<?php
+//include('oqey-functions.php');
+global $wpdb;
+$oqey_music = $wpdb->prefix . "oqey_music";
+
+if(isset($_GET['scaner'])){
+	$root = str_replace('\\', '/', ABSPATH)."wp-content/oqey_gallery/music";
+
+if(is_dir($root)){
+
+$files = php4_scandir($root);
+
+//$files = get_mp3_files($root);
+	
+global $wpdb;
+$oqey_music = $wpdb->prefix . "oqey_music";
+$root = $root."/";
+$d=0;
+
+foreach($files as $file){
+
+$path = pathinfo($file);
+
+if(!isset($path['filename'])){
+    $audio_title = trim(sanitize_title(substr($path['basename'], 0,strpos($path['basename'],'.')))).".".$path['extension'];
+	$music_title = substr($path['basename'], 0,strpos($path['basename'],'.'));
+}else{
+    $audio_title = trim(sanitize_title($path['filename']).".".$path['extension']);//denumirea imaginii
+	$music_title = $path['filename'];
+}
+
+
+$ext = array('mp3', 'MP3'); 
+if (in_array( strtolower($path['extension']), $ext) ){	
+
+if(!$sql=$wpdb->get_row($wpdb->prepare("SELECT * FROM $oqey_music WHERE link = %s", $audio_title))){
+$wpdb->query("INSERT INTO $oqey_music (link, title) VALUES ('$audio_title', '$music_title')");
+rename($root.$file, $root.$audio_title);
+$d++;
+}
+}
+
+}
+$mesaj = '<p class="updated fade">'.$d.' new audio files found.</p>';
+}else{
+$mesaj = '<p class="error">Error creating the default gallery music folder (might be a server restrictions issue). Please create this folder manually (wp-content/oqey_gallery/music) and set permissions to 777.</p>';
+}
+}
+?>    
+<script type="text/javascript">
+function clearUp(){ setTimeout(function () { jQuery('#save').fadeOut(function(){ jQuery("#save").html("&nbsp;"); }); }, 2000); }
+
+function deleteSong(id){ 
+jQuery.post("<?php echo oQeyPluginUrl(); ?>/do.php", { songtotrash: id },
+   function(id){
+            jQuery("#row_" + id).fadeOut('slow');
+			jQuery("#save").hide().html('<p class="updated fade">Song was moved to trash. <a href="#undo" id="restoresong">undo<\/a><\/p>').fadeIn("slow"); 
+			
+			jQuery("#restoresong").click(function(){
+		    jQuery.post("<?php echo oQeyPluginUrl(); ?>/do.php", { restoresong: id },
+            function(data){			
+			jQuery("#row_" + id).fadeIn('slow');			
+			jQuery("#save").hide().html('<p class="updated fade">Song was restored.<\/p>').fadeIn("slow"); 
+			});
+            clearUp();
+            });
+
+   });
+}
+
+function hoverSongs(){
+jQuery("#sortable tr").hover(
+  function () {
+  jQuery(this).children(".lasttd").children(".hiddenm").addClass("visiblem"); 
+  },
+  function () {
+  jQuery(this).children(".lasttd").children(".hiddenm").removeClass("visiblem").addClass("hiddenm");
+  }
+);
+}
+
+jQuery(document).ready(function($) {	
+
+jQuery.loadImages([ '<?php echo oQeyPluginUrl().'/images/preview_button.png'; ?>', '<?php echo oQeyPluginUrl().'/images/remove_button.png'; ?>', '<?php echo oQeyPluginUrl().'/images/edit_button.png'; ?>' ],function(){});
+
+hoverSongs();
+
+jQuery(".click").editable("<?php echo oQeyPluginUrl(); ?>/save_music_details.php", { 
+      indicator : 'Updating...',
+      tooltip   : "Double-click to rename...",
+      event     : "dblclick",
+      style  : "inherit",
+	  width : "80%",
+	  height : "15px"
+  });
+
+});
+	var name=1;
+	var firstplay = true;
+	function checkActivePlayer(newname) {
+		if (!firstplay) {
+        	getFlashMovie("playbtn" + name).sendIDToFlash(newname);
+			name = newname;
+		} else {
+			name = newname;
+			firstplay = false;
+		}
+	}
+	function getFlashMovie(movieName) {
+		var isIE = navigator.appName.indexOf("Microsoft") != -1;   return (isIE) ? window[movieName] : document[movieName];
+	}
+</script> 
+<div class="wrap">
+        <h2>Manage Music</h2>
+<?php 
+  $gal_dir_musica = str_replace('\\', '/', ABSPATH)."/wp-content/oqey_gallery/music";   
+  if(!is_dir($gal_dir_musica)){
+  $eroarea = "Error creating the default gallery music folder (might be a server restrictions issue). Please create this folder manually (wp-content/oqey_gallery/music) and set permissions to 777.";
+  echo '<div class="wrap"><div class="error" id="error"><p>' . $eroarea . '</p></div></div>';
+  }
+  ?>
+
+<div id="save" style="width:800px; margin-bottom:10px; margin-left:-5px;"><?php echo $mesaj; ?></div>
+</div> 
+
+
+
+<div class="postbox" style="height:50px; width:800px;">
+<table width="800" border="0" cellspacing="0" cellpadding="5">
+  <tr>
+    <td width="225" height="50" align="right" valign="middle">Upload a new audio file (*.mp3)</td>
+    <td width="125" height="50" align="right" valign="middle">
+    
+<div id="flashuploader" style="width:100px; height:30px;"></div>	   
+    <script type="text/javascript">
+	var flashvars = {BatchUploadPath:"<?php echo str_replace('\\', '/', ABSPATH); ?>wp-content/oqey_gallery/music",
+					 Handler:"<?php echo oQeyPluginUrl(); ?>/bmupload.php",
+					 FTypes:"*.mp3",
+					 FDescription:"Media Files"};
+	var params = {bgcolor:"#FFFFFF",
+				  allowFullScreen:"true", wMode:"transparent"};
+	var attributes = {id: "flash"};
+	swfobject.embedSWF("<?php echo oQeyPluginUrl(); ?>/demoupload.swf", "flashuploader", "110", "30", "8.0.0", "", flashvars, params, attributes);
+</script>
+
+</td>
+    <td width="20" height="50">/</td>
+    <td width="430" height="50" align="left"> 
+    <div align="left" style="padding-top:3px;">
+    <form id="scaner" name="scaner" method="post" action="<?php echo get_option('siteurl'); ?>/wp-admin/admin.php?page=oQeyMusic&amp;scaner=true">     
+        <input type="submit" name="scanfolder" id="scanfolder" value="refresh" />              
+    </form>
+    </div>
+    </td>
+  </tr>
+</table>
+</div>
+
+<div class="postbox" style="width:800px;">
+<table width="800" border="0" cellspacing="0" cellpadding="3" id="musictable" class="tablesorter">
+<tbody id="sortable">
+<?php
+global $wpdb;
+$oqey_music = $wpdb->prefix . "oqey_music";
+	$i=1;
+	$get_music = $wpdb->get_results("SELECT * FROM $oqey_music WHERE status !=2");
+	$message = "Are you sure you want to delete this song?";	
+	$totalFiles = count($get_music);	
+	foreach ($get_music as $music){
+	if($i%2){ $tr_bg = "background-color:#CECECE;"; }else{ $tr_bg = "background-color:#E4E4E4;"; }
+?>
+  <tr style="padding:3px;" id="row_<?php echo $music->id; ?>">
+    <td align="center" width="50" style="height:35px;">
+    <div id="flashm<?php echo $i; ?>">
+    <script type="text/javascript">
+	var flashvars = {mpath:"<?php echo get_option('siteurl').'/wp-content/oqey_gallery/music/'.$music->link;  ?>", flashId:"<?php echo $i; ?>", totalFiles:"<?php echo $totalFiles; ?>"};
+	var params = {wMode:"transparent"};
+	var attributes = {id: "playbtn<?php echo $i; ?>"};
+	swfobject.embedSWF("<?php echo oQeyPluginUrl(); ?>/musiccs.swf", "flashm<?php echo $i; ?>", "30", "30", "8.0.0", "", flashvars, params, attributes);
+    </script>
+    </div>   
+    </td>
+    <td align="left" width="725" >
+    <div class="click" id="select_<?php echo $music->id; ?>"><?php echo $music->title; ?></div>    
+    </td>
+    <td width="30" align="center" valign="middle" class="lasttd"> 
+    <a href="#null" onClick="deleteSong('<?php echo $music->id; ?>'); return false;" class="hiddenm">
+    <img src="<?php echo oQeyPluginUrl(); ?>/images/remove_button.png" width="24" height="24" title="Click to move to trash this song"/></a>
+    </td>
+  </tr>
+<?php 
+$i++;
+} ?>
+  </tbody>
+</table>
+</div>
+
+<div class="postbox" style="width:800px;">
+<div align="left" style="margin:15px;">
+Notes: <br />
+         *<?php echo uploadSize(); ?><br />
+         *You may upload new audio files directly to your plugin audio directory via ftp.<br />
+         *Your audio folder location:<b><?php echo get_option('siteurl').'/wp-content/oqey_gallery/music/'; ?></b>       
+    </div>
+</div>
+
+<script type="text/javascript">
+//uploader use this function
+function refreshPage(){ window.location = "<?php echo get_option('siteurl'); ?>/wp-admin/admin.php?page=oQeyMusic"; }
+</script>
---
+++ readme.txt
@@ -0,0 +1,108 @@
+=== Plugin Name ===
+Plugin Name: oQey Gallery
+Version: 0.3.2
+Contributors:  oQeySites.com
+Donate link: http://oqeysites.com/donations-page/
+Tags:  custom gallery, photos,flash,slideshow,images,gallery, photo,media ,gallery in post,photo-albums, custom slideshow, picture, pictures,image, skinnable gallery,oqey-gallery, custom flash, oqey gallery, wp custom slideshow, slideshow with music suport, gallery with music
+Requires at least: 3.0.0
+Tested up to: 3.0.0
+Stable tag: 0.3.2
+
+== Description ==
+oQey Gallery is a Plugin for Wordpress that let users create and manage flash slideshows with a non-flash version of gallery built-in for all non-flash browsers and mobile devices like iPhone / iPad / iPod etc. The flash version supports music and skins, so users can change the way it looks with a few simple clicks. Customizable, commercial skins are also available as well as custom galleries for professionals. This plugin uses built-in WP functions and a simple batch upload system. Multiple galleries are supported.
+
+Links:
+
+*	<a href=" http://oqeysites.com/oqey-flash-gallery-plugin/" title="Demo gallery">Demo Gallery</a>
+*	<a href=http://oqeysites.com/oqey-flash-gallery-plugin/oqey-gallery-faq/ title="FAQ">oQey Gallery FAQ</a> 
+ 
+
+For more details, skins and examples and custom flash gallery, please visit 
+www.oqeysites.com 
+
+
+Features:
+
+* Simple and intuitive gallery management
+* Built-in flash slideshow with a simple music player
+* Skinnable flash slideshow
+* Free skins available and many more coming up
+* Batch media upload
+* Works with any wp theme
+* Customizable slideshow size
+* Drag & drop to sort images
+* Custom skins on demand
+* Insert in posts / pages with a single click
+* iPhone / iPad detection
+* Fullscreen support
+* Advanced SEO tools for indexing photos
+* Multiple play control - if you press play another instance of a slideshow in a page, 
+  the started slideshow will stop playing
+* Custom logo support - commercial skins
+* Flash Watermark support  
+
+
+== Installation ==
+1. Unzip the plugin archive and put oqey-gallery  folder into your plugins directory (wp-content/plugins/)
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= oQey Gallery plugin need a special setup? =
+No.
+
+= How is must be set safe_mode? =
+oQey Gallery plugin works fine with safe_mode=Off only. Please contact you server administrator 
+in order to switch safe_mode to 'off', if it is 'on'.
+ 
+= How can I change the image size for the no flash version of my gallery? =
+Just edit css/oqeystyle.css and make all changes that you need.
+
+
+== Screenshots ==
+
+1. oQey Gallery Edit
+2. oQey Gallery Settings
+3. oQey Gallery Music management
+4. oQey Gallery Flash Preview
+5. oQey Gallery Skin management
+6. oQey Gallery Insert Button
+7. oQey Gallery Gallery List
+
+
+
+== Changelog ==
+= 0.3.2=
+* This version just fixes a few minor bugs.
+
+= 0.3=
+* This version just fixes a few minor bugs.
+* Flash slideshow size limits lifted
+* Flash thumbnails auto-hide function added
+* Flash photo resizing issues fixed
+* Thumbnails hide option added to gallery settings
+
+= 0.2=
+* This version just fixes a few minor bugs. 
+
+
+= 0.1=
+* The first stable version.
+
+
+== Upgrade Notice ==
+= 0.3.2=
+* This version just fixes a few minor bugs.
+
+= 0.3=
+* This version just fixes a few minor bugs.
+* Flash slideshow size limits lifted
+* Flash thumbnails auto-hide function added
+* Flash photo resizing issues fixed
+* Thumbnails hide option added to gallery settings
+
+= 0.2=
+* This version just fixes a few minor bugs. 
+
+= 0.1=
+* The first stable version.
