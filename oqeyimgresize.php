cd56a5022b68c0b0ac1248a998a53a712da1caa3
dariimd <dariimd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-21 10:22:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/oqey-gallery/tags/0.4.8.4@441403 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ oqeyimgresize.php
@@ -0,0 +1,45 @@
+<?php
+require_once("../../../wp-load.php");
+
+if($_REQUEST['img_type']=="nextgen"){
+    $path = OQEY_ABSPATH.'/'.urldecode($_REQUEST['img_f_path']).'/';
+    $img  = $path.$_REQUEST['img'];    
+}else{
+    $path = OQEY_ABSPATH.'wp-content/oqey_gallery/galleries/'.oqey_getBlogFolder($wpdb->blogid).$_REQUEST['folder'];
+    $img  = $path . '/iphone/' . $_REQUEST['img'];
+}
+
+if (file_exists($img)) {
+	$width = $_REQUEST['width'];
+	$height = $_REQUEST['new_height'];
+
+$x = @getimagesize($img);
+if($x){
+$oqeyw = $x[0];
+$oqeyh = $x[1];
+}
+
+
+$im = ImageCreateFromJPEG ($img) or 
+$im = ImageCreateFromPNG ($img) or 
+$im = false;
+
+if (!$im) {
+   /* 
+    header ("Content-type: image/jpeg");
+	readfile ($img);
+    */
+    $fisier = str_replace(OQEY_ABSPATH, get_option("siteurl")."/", $img);
+    header('Location: '.$fisier);
+    exit();
+    
+} else {
+    header ("Content-type: image/jpeg");
+	$thumb = @ImageCreateTrueColor ($width, $height);
+	@ImageCopyResampled ($thumb, $im, 0, 0, 0, 0, $width, $height, $oqeyw, $oqeyh);
+	@ImageJPEG ($thumb);
+    @imagedestroy($thumb);
+}
+
+}
+?>
