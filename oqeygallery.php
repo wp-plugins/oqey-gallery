bb5e6bec4b1f46e833b134b3d6b76bdeeb38b0de
dariimd <dariimd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-17 14:57:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/oqey-gallery/tags/0.5@452061 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- oqeygallery.php
+++ oqeygallery.php
@@ -125,7 +125,7 @@ function oqey_db_install(){
     $sql4 = "CREATE TABLE `" . $oqey_music . "` (
 		`id` int(11) NOT NULL AUTO_INCREMENT,
 		`link` varchar(255) NOT NULL DEFAULT '',
-        `path` varchar(255) NOT NULL DEFAULT ''
+        `path` varchar(255) NOT NULL DEFAULT '',
 		`title` tinytext NOT NULL,
 		`artist` varchar(255) NOT NULL DEFAULT '',
 		`music_order` int(11) NOT NULL DEFAULT '0',
